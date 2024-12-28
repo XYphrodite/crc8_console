48c05ba5abfb8b0d1d7ae851fb43a665080803ab
Maksim Meshcheryakov <xyphrodite@gmail.com>
2024-12-28 12:33:14 +0300

    init

---
+++ crc8_console/Program.cs
@@ -0,0 +1,98 @@
+﻿
+
+
+using System.Data.SqlTypes;
+using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Type packet in hexadecimal without spaces...");
+        string? packet_str = Console.ReadLine();
+#if DEBUG
+        Console.WriteLine("You type:\t" + packet_str);
+#endif
+        Console.WriteLine("Try parse...");
+
+        if (packet_str?.Length % 2 != 0)
+        {
+            Console.WriteLine(">>\tSize of packet can't be odd!");
+            Console.ReadLine();
+            return;
+        }
+
+        List<byte> bytes = new();
+        try
+        {
+            for (int i = 0; i < packet_str?.Length; i += 2)
+            {
+                bytes.Add(byte.Parse(packet_str.Substring(i, 2), NumberStyles.HexNumber, CultureInfo.InvariantCulture));
+            }
+        }
+        catch (Exception e)
+        {
+
+        }
+
+        foreach (var b in bytes)
+            Console.Write(b + " ");
+        Console.WriteLine();
+
+        int size_32 = bytes.Count / 2 + bytes.Count % 2 != 0 ? 1 : 0;
+        var upd_bytes = new List<byte>();
+
+        for (int i = 0; i < bytes.Count; i += 4)
+        {
+            var diff = bytes.Count - i;
+            if (diff - i >= 4)
+            {
+                upd_bytes.Add(bytes[i + 3]);
+                upd_bytes.Add(bytes[i + 2]);
+                upd_bytes.Add(bytes[i + 1]);
+                upd_bytes.Add(bytes[i + 0]);
+            }
+            else
+            {
+                var diff2 = 4 - diff;
+                var _diff = diff;
+                while (diff-- > 0)
+                    upd_bytes.Add(0x00);
+
+                while (diff2-- > 0)
+                    upd_bytes.Add(bytes[i + diff2]);
+
+            }
+        }
+
+        Console.Write("Packet:\t\t");
+        foreach (var ub in upd_bytes)
+            Console.Write(ub + " ");
+        Console.WriteLine();
+
+        byte crc = crc8x_simple(0xff, upd_bytes);
+        Console.WriteLine("CRC:\t\t" + crc);
+
+        Console.Write("Command:\t");
+        foreach (var b in bytes)
+            Console.Write("{0:X2} ",b);
+        Console.WriteLine("{0:X2}", crc);
+
+        Console.WriteLine("Press any key...");
+        Console.ReadLine();
+    }
+
+    static byte crc8x_simple(uint crc, List<byte> packet)
+    {
+        if (packet.Count == 0)
+            return 0xff;
+        for (int i = 0; i < packet.Count; ++i)
+        {
+            crc ^= packet[i];
+            for (byte k = 0; k < 8; k++)
+                crc = (crc & 0x80) > 0 ? (crc << 1) ^ 0xb7 : crc << 1;
+            crc &= 0xff;
+        }
+        return (byte)crc;
+    }
+}
